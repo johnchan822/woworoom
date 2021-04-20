7df00dee06993bab1dde3dccd85761d620b08e38
johnchan822 <45100631+johnchan822@users.noreply.github.com>
2021-04-20 22:12:49 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,194 +1,194 @@
+let data =[];
+let cartData =[];
+let total;
+let userUrl =`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/johnchan/`
+let adminUrl =`https://hexschoollivejs.herokuapp.com/api/livejs/v1/admin/johnchan/`
+init();
 
-let data = [];
-let dataCart = [];
-
-function init() {
-  getProduct()
+function init(){
+  getProduct();
   getProductCart();
 }
-function getProduct() {
-  axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/products`)
+function getProduct(){
+  axios.get(userUrl +`products`)
+  .then(function(response){
+    // console.log(response.data.products)
+    data = response.data.products;
+    printList(data);
+    printSelectFilter();
+  })
+}
+function printList(allData) {
+  // console.log(data)
+  let str ='';
+  allData.forEach(item =>{
+    str +=` <li class="productCard">
+          <h4 class="productType">新品</h4>
+          <img src="${item.images}"
+            alt="">
+          <a href="#" id="addCardBtn" data-id=${item.id} data->加入購物車</a>
+          <h3>${item.title}</h3>
+          <del class="originPrice">${item.origin_price}</del>
+          <p class="nowPrice">${item.price}</p>
+        </li>`
+  })
+  document.querySelector('.productWrap').innerHTML = str;
+}
+function getProductCart(){
+    // console.log(cartData)
+    axios.get(userUrl +`carts`)
+      .then(function (response) {
+        cartData =response.data.carts;
+        total = response.data.finalTotal;
+        // console.log(total)
+        printCartList()
+      })
+}
+function printCartList() {
+  // console.log(data)
+  console.log(cartData)
+  let str = '';
+  cartData.forEach(item => {
+    str += `<tr>
+          <td>
+            <div class="cardItem-title">
+              <img src="https://i.imgur.com/HvT3zlU.png" alt="">
+              <p>${item.product.title}</p>
+            </div>
+          </td>
+          <td>${item.product.price}</td>
+
+          <td class="quantityEdit">
+          <button class="lessBtn" data-num=${item.quantity - 1} data-id=${item.id}>-</button>
+          ${item.quantity}
+          <button class="plusBtn" data-num=${item.quantity + 1} data-id=${item.id}>+</button>
+          </td>
+          <td>${item.product.price * item.quantity}</td>
+          <td class="discardBtn">
+            <a href="#" class="material-icons " id="delBtn" data-id=${item.id} >
+              clear
+            </a>
+          </td>
+        </tr>`
+  })
+  document.querySelector('.totalPrice').textContent = total;
+  document.querySelector('.table').innerHTML = str;
+}
+function printSelectFilter() {
+  //  console.log(data)
+  //第一次篩選出所有的品項
+  let dataSelect = data.map((item) => {
+    return item.category;
+  })
+  //第二次篩選將同名稱的品項汰換
+  // console.log(dataSelect)
+  let select = dataSelect.filter((item, i) => {
+    return dataSelect.indexOf(item) === i;
+  })
+  //將select重新接回去
+  str = ``;
+  select.forEach((item) => {
+    str += `<option value="${item}">${item}</option>`
+  })
+  firstStr = `<option value="全部">全部</option>`
+  document.querySelector('.productSelect').innerHTML = firstStr + str;
+
+}
+
+function dellCartAllProduct(e){
+  e.preventDefault();
+  axios.delete(userUrl+`carts`)
     .then(function (response) {
-      data = response.data.products
-      // console.log(data)
-      printList(data);
+    alert('以清除購物車')
+      init()
+
+  })
+    .catch(function (errors) {
+      alert("已無商品")
     })
 }
-function printList(forData) {
-  let str = '';
-  forData.forEach(item => {
-    str += `
-      <li class="productCard">
-        <h4 class="productType">新品</h4>
-        <img
-          src=${item.images}
-          alt="">
-        <a href="#" id="addCardBtn" data-id=${item.id} data-title=${item.title.replace(/\s+/g, '')}>加入購物車</a>
-        <h3>${item.title}</h3>
-        <del class="originPrice">${toThousands(item.origin_price)}</del>
-        <p class="nowPrice">${toThousands(item.price)}</p>
-      </li>`
-  })
-  document.querySelector(".productWrap").innerHTML = str;
-}
-function selectChange(){
-  let select = document.querySelector(".productSelect").value;
-  let filterData = data.filter((item) => {
-    if (item.category == select) {
+function selectProduct(e) {
+  // console.log(data)
+  let productSelectValue = document.querySelector('.productSelect').value;
+  let filterData = data.filter((item)=>{
+    if (productSelectValue == item.category){
       return item;
     }
-    else if (select == "全部") {
+    else if (productSelectValue =="全部"){
       return item;
     }
   })
   printList(filterData)
 }
-function getProductCart(){
-  axios.get(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/carts`)
-    .then(function (response) {
-      dataCart = response.data.carts;
-      // console.log(dataCart)
-      let total = response.data.finalTotal;
-      let str = '';
-      dataCart.forEach(item => {
-        str +=
-          ` <tr>
-          <td>
-            <div class="cardItem-title">
-              <img src=${item.product.images}>
-              <p>${item.product.title}</p>
-            </div>
-          </td>
-          <td>${toThousands(item.product.price)}</td>
-          <td><input type="number" class="quantity" value='${item.quantity}' data-num="${item.id}"></td>
-          <td>${toThousands(item.quantity * item.product.price)}</td>
-          <td class="discardBtn">
-            <a href="#" class="material-icons " id="delectBtn" data-title=${item.product.title.replace(/\s+/g, '')} data-id=${item.id}>
-              clear
-            </a>
-          </td>
-        </tr>
-        <tr>
-    `
-      })
-      document.querySelector(".tbody").innerHTML = str;
-      document.querySelector(".totalPrice").textContent = toThousands(total);
 
-
-    })
-}
-function addProductCart(e){
+function addProduct(e){
   e.preventDefault();
-  let selectId = e.target.dataset.id;
-  let selectTitle = e.target.dataset.title;
-  if (e.target.getAttribute("id") !== "addCardBtn") {
-    return;
+  if (e.target.getAttribute('id') !=='addCardBtn'){
+    return ;
   }
-  let numCheck = 1;
-  dataCart.forEach(item => {
-    if (item.product.id === selectId) {
+  //這段要注意!!
+  let productId = e.target.dataset.id;
+  let numCheck =1;
+    cartData.forEach(function (item) {
+    if (item.product.id === productId) {
       numCheck = item.quantity += 1;
     }
   })
-  //這邊使用post!!
-  axios.post(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/carts`, {
+  axios.post(`${userUrl}carts`, {
     "data": {
-      "productId": selectId,
+      "productId": productId,
       "quantity": numCheck
-    }
-  })
-    .then(function () {
-      alert(`"${selectTitle}"加入購物車`)
+    }}).then(function() {
       init();
+      alert('已加入到購物車')
     })
 }
-function delAllProductCart(e){
-  e.preventDefault();
-  axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/carts`)
-    .then(function () {
-      init();
-      alert("以清除購物車")
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 
-}
-function editProduct(e){
+function Product(e) {
   e.preventDefault();
   let productId = e.target.dataset.id;
-  let productTitle = e.target.dataset.title;
-  if (e.target.getAttribute("id") == "delectBtn") {
-    axios.delete(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/carts/${productId}`)
-      .then(function () {
-        alert(`已刪除${productTitle}品項`)
-        init();
-      })
+  let num = e.target.dataset.num;
+  if (e.target.getAttribute('id') =='delBtn'){
+    delProduct(productId);
   }
-  if(e.target.getAttribute("class") == "quantity"){
-    let numId = e.target.dataset.num;
-    let num = parseInt(e.target.value);
-
-    if (e.target.value < 1) {
-      alert('商品數量必須大於1');
-      e.target.value = 1;
-      return;
+  if (e.target.getAttribute('class') == 'lessBtn' || e.target.getAttribute('class') == 'plusBtn'){
+    if(num <1){
+      alert('購物車商品不能小於0')
+      return
     }
-    axios.patch(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/carts`, {
-      "data": {
-        "id": numId,
-        "quantity": num
-      }
-    }).then(function (response) {
-      // alert('修改商品數量成功');
-      init();
-    }).catch(function (error) {
-      console.log(error);
-    })
+    editProduct(productId, num);
   }
-
 }
-function formSumbit(e) {
-  e.preventDefault();
-  let name = document.querySelector("#customerName").value;
-  let phone = document.querySelector("#customerPhone").value;
-  let email = document.querySelector("#customerEmail").value;
-  let address = document.querySelector("#customerAddress").value;
-  let trade = document.querySelector("#tradeWay").value;
-  let errors = validate(form, constraints);
+function delProduct(productId){
 
-  if (dataCart.length == 0) {
-    alert("麻煩將商品加入到購物車喔!")
-    // return
-  }
-  if (errors) {
-
-    checkErrors();
-    return;
-  }
-  axios.post(`https://hexschoollivejs.herokuapp.com/api/livejs/v1/customer/${apiKey}/orders`, {
-    "data": {
-      "user": {
-        "name": name,
-        "tel": phone,
-        "email": email,
-        "address": address,
-        "payment": trade
-      }
-    }
-  }).then(function () {
-    alert('已送出表單');
-    document.querySelector("#customerName").value = "";
-    document.querySelector("#customerPhone").value = "";
-    document.querySelector("#customerEmail").value = "";
-    document.querySelector("#customerAddress").value = "";
-    document.querySelector("#tradeWay").value = "ATM";
+  axios.delete(`${userUrl}carts/${productId}`)
+  .then(function(response) {
+    alert("已刪除此筆訂單")
+    init();
   })
-    .catch(function (error) {
-      console.log(error);
-    });
+  .catch(function(respnse){
+    alert('錯誤')
+  })
 }
-const inputs = document.querySelectorAll("input[type=text],input[type=tel],input[type=email]");
+function editProduct(productId,num){
+ 
+  axios.patch(`${userUrl}carts`, 
+  {
+    "data": {
+      "id": productId,
+      "quantity": parseInt(num)
+    }
+  }).then(function(response){
+    // console.log(response)
+    init();
+  })
+  .catch(function(response){
+    alert('錯誤')
+  })
+}
+
+
+const inputs = document.querySelectorAll("input[name],select[data=payment]");
 const form = document.querySelector(".orderInfo-form");
 const constraints = {
   "姓名": {
@@ -225,39 +225,71 @@ const constraints = {
   },
 };
 
-  //認證機制
-  inputs.forEach(function (item) {
-    item.addEventListener("blur", function (e) {
-      item.nextElementSibling.textContent = " ";
-      let errors = validate(form, constraints) || "";
-      item.nextElementSibling.textContent =
-        errors[e.target.getAttribute("name")];
-    });
+//認證
+
+inputs.forEach((item) => {
+  item.addEventListener("change", function () {
+    let errors = validate(form, constraints);
+    item.nextElementSibling.textContent = '';
+    // console.log(errors)
+
+    if (errors) {
+      Object.keys(errors).forEach(function (keys) {
+        document.querySelector(`[data-message="${keys}"]`).textContent = errors[keys];
+        document.querySelector('.orderInfo-btn').disabled = false;
+        // alert('麻煩填寫正確')
+      })
+    }
   });
-  //
-  function checkErrors() {
-    let errors = validate(form, constraints) || "";
-    let list = Object.keys(errors);
-      console.log(errors)
-      list.forEach(function (keys) {
-    document.querySelector(`[data-message="${keys}"]`).textContent = errors[keys];
-  });
-}
-  getProduct();
-  getProductCart();
-  document.querySelector(".productWrap").addEventListener('click', addProductCart)
-  document.querySelector('.discardAllBtn').addEventListener('click', delAllProductCart)
-  document.querySelector(".productSelect").addEventListener('change', selectChange)
-  document.querySelector(".tbody").addEventListener("click", editProduct)
-  document.querySelector(".orderInfo-btn").addEventListener("click", formSumbit)
+});
 
 
-
-
-
-  // util js、元件
-  function toThousands(x) {
-    let parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return parts.join(".");
+function sumbitOrder(e){
+e.preventDefault();
+  let errors = validate(form, constraints);
+  if (cartData.length == 0) {
+    alert("麻煩將商品加入到購物車喔!")
+    return
   }
+  if (errors){
+    alert('麻煩填寫正確')
+    return;
+  }
+  let name = document.querySelector('#customerName').value;
+  let tel = document.querySelector('#customerPhone').value;
+  let email = document.querySelector('#customerEmail').value;
+  let address = document.querySelector('#customerAddress').value;
+  let tradeWay = document.querySelector('#tradeWay').value;
+
+  axios.post(userUrl + `orders`, {
+    "data": {
+      "user": {
+        "name": name,
+        "tel": tel,
+        "email": email,
+        "address": address,
+        "payment": tradeWay
+      }
+    }
+  }).then(function (response) {
+    // console.log(response)
+    alert('已送出訂單')
+    document.querySelector("#customerName").value = "";
+    document.querySelector("#customerPhone").value = "";
+    document.querySelector("#customerEmail").value = "";
+    document.querySelector("#customerAddress").value = "";
+    document.querySelector("#tradeWay").value = "ATM";
+
+  }).catch(function(response){
+    // console.log(response)
+  })
+
+
+}
+
+document.querySelector('.table').addEventListener('click',Product)
+document.querySelector('.productWrap').addEventListener('click',addProduct)
+document.querySelector('.discardAllBtn').addEventListener('click',dellCartAllProduct)
+document.querySelector('.productSelect').addEventListener('change',selectProduct)
+document.querySelector('.orderInfo-btn').addEventListener('click',sumbitOrder)
+
